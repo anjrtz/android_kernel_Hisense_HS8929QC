3a040b56fc5709f87f93d9a56b4b64f32624cf00
subekti cahyo <subekti.cahyo@gmail.com>
2016-06-12 22:36:43 +0700

    remove unused file

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Jun 12 09:31:32 WIB 2016"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Jun 12 18:12:16 WIB 2016"
 #define LINUX_COMPILE_BY "bekt"
 #define LINUX_COMPILE_HOST "unknown"
 #define LINUX_COMPILER "gcc version 5.1.1 20150608 (Linaro GCC 5.1-2015.08) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.49-perf"
+#define UTS_RELEASE "3.10.49-perf-gb984348-dirty"
