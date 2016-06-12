b98434896c2e09103a1b10a9d968790a51888a88
Subekti Cahyo <subekti.cahyo@gmail.com>
2016-06-12 13:35:55 +0700

    first commit

---
+++ envsetup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export ARCH=arm64
+export SUBARCH=arm64
+export CROSS_COMPILE=/media/bekt/a15cbea9-28d2-46e0-998d-9beb5eda65a8/gcc-linaro-5.1-2015.08-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
